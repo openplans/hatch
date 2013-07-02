2d5587869d1b1823c93b0a396056f72ade5f1695
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-02 13:38:25 -0400

    Add simple login/out link to header

--- src/visionlouisville/urls.py
+++ src/visionlouisville/urls.py
@@ -8,6 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # App
     url(r'^$', app_view, name='app'),
+    url(r'^', include('social_auth.urls')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', 
+        kwargs={'next_page': '/'}),
 
     # API
     url(r'^api/', include(api_router.urls)),
