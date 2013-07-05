1f61d1428aab33c8e65e3fac0e92ed9cdd7b65b9
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-05 19:15:44 -0400

    Add a UserSerializer

--- src/visionlouisville/serializers.py
+++ src/visionlouisville/serializers.py
@@ -1,17 +1,25 @@
-from rest_framework.serializers import ModelSerializer, URLField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, URLField
 from .models import User, Vision
 
 
 class UserSerializer (ModelSerializer):
-    avatar_url = URLField()
+    avatar_url = SerializerMethodField('get_avatar_url')
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'avatar_url')
+        fields = ('id', 'username', 'first_name', 'last_name', 'avatar_url')
+
+    def get_avatar_url(self, obj):
+        return obj.avatar_url
 
 
 class VisionSerializer (ModelSerializer):
-    author = UserSerializer()
+    author_avatar_url = SerializerMethodField('get_avatar_url')
 
     class Meta:
         model = Vision
+
+    def get_avatar_url(self, obj):
+        return obj.author.avatar_url
+        # twitter_service = self.context['twitter_service']
+        # return twitter_service.get_avatar_url(obj.author_pk)
