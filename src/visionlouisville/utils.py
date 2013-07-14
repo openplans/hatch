3b7c6d44fa4df7927e431b7e3e6fdba537ac3989
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-14 04:53:57 -0400

    Add a method to uniquify tweet ids

--- src/visionlouisville/utils.py
+++ src/visionlouisville/utils.py
@@ -1,8 +1,21 @@
-from itertools import islice
+from itertools import combinations, islice
+from random import randint
+
+
+def uniquify_tweet_ids(queryset):
+    """Ensure that the tweet_ids of all items in the queryset are unique"""
+    all_different = False
+    while not all_different:
+        all_different = True
+        for referrence, other in combinations(queryset, 2):
+            if reference.tweet_id == other.tweet_id:
+                all_different = False
+                other.tweet_id = str(randint(0, 9999999999999999))
+                other.save()
 
 
 def chunk(iterable, n):
-    "Collect data into fixed-length chunks"
+    """Collect data into fixed-length chunks"""
     it = iter(iterable)
     while True:
         item = list(islice(it, n))
