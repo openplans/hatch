b991171f734bf5a21d9d41165e2d8fbd39133f37
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-18 14:15:08 -0400

    Cleanup

--- src/visionlouisville/tasks.py
+++ src/visionlouisville/tasks.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from celery import task, group
+from celery import task
 from time import sleep
 from .models import User, Moment
 from .utils import chunk
 from .services import default_twitter_service as twitter_service
 
-import logging 
+import logging
 log = logging.getLogger(__name__)
 
 
@@ -21,23 +21,27 @@ def refresh_users():
             log.info('\n  - Waiting 60 seconds before next group\n')
             sleep(60)
 
-        log.info('\n  - Downloading info for group of %s user(s)\n' % (len(user_group),))
+        log.info('\n  - Downloading info for group of %s user(s)\n' %
+                 (len(user_group),))
         twitter_service.get_users_info(user_group)
         first_group_done = True
 
     log.info('\n*** Done refreshing the user cache. Run me again in a day!\n')
+
 
 @task
 def listen_for_moments():
 
     log.info('\n*** Listening for moments...\n')
 
-    tweets = twitter_service.itertweets(track=','.join(settings.MOMENT_KEYWORDS))
+    moment_keywords = ','.join(settings.MOMENT_KEYWORDS)
+    tweets = twitter_service.itertweets(track=moment_keywords)
     for tweet in tweets:
         if 'disconnect' in tweet:
-            log.info("\n*** Twitter doesn't like you anymore: %s (%s)\n" % 
-                     (tweet['disconnect'].get('reason'), 
-                      tweet['disconnect'].get('code')))
+            msg = tweet['disconnect']
+            log.info(
+                "\n*** Twitter doesn't like you anymore. Reason: %s (%s)\n" %
+                (msg.get('reason'), msg.get('code')))
             break
 
         log.info('\n  - I see a tweet!: %s\n' % (tweet.get('id', str(tweet)),))
@@ -48,6 +52,6 @@ def listen_for_moments():
             if created:
                 log.info('\n  - Created a new moment "%s"\n' % (moment,))
             else:
-                log.info('\n  - Modified an existing moment "%s"\n' % (moment,))
+                log.info('\n  - Modified a moment "%s"\n' % (moment,))
         else:
             log.info('\n  - Nevermind, not interested.\n')
