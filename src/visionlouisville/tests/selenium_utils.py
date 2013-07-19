bd2f872a049cb2236ebf823559a7d1279662af46
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-07-19 00:52:08 -0400

    Set additional default options for selenium tests, allowing either BrowserStack or SauceLabs

--- src/visionlouisville/tests/selenium_utils.py
+++ src/visionlouisville/tests/selenium_utils.py
@@ -1,9 +1,14 @@
 from django.conf import settings
+from django.test import TestCase
 from nose.tools import nottest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from functools import wraps
 from os import environ
+
+
+class SeleniumTestCase (TestCase):
+    base_url = 'http://localhost:8000'
 
 
 # ============================================================
@@ -30,37 +35,67 @@ def selenium_test(func, **options):
     # Set up defaults and process the options
     options.setdefault('name', func.__name__)
     options.setdefault('javascriptEnabled', True)
-    options.setdefault('build', environ['TRAVIS_BUILD_NUMBER'])
-    options.setdefault('tags', [environ['TRAVIS_PYTHON_VERSION'], 'CI'])
-    options.setdefault('tunnel-identifier', environ['TRAVIS_JOB_NUMBER'])
+
+    is_saucelabs = 'saucelabs' in settings.SELENIUM_COMMAND_EXECUTOR
+    is_browserstack = 'browserstack' in settings.SELENIUM_COMMAND_EXECUTOR
+
+    # For SauceLabs
+    if is_saucelabs:
+        options.setdefault('build', environ.get('TRAVIS_BUILD_NUMBER', 'latest'))
+        options.setdefault('tags', [environ.get('TRAVIS_PYTHON_VERSION', 'latest'), 'CI'])
+        options.setdefault('tunnel-identifier', environ.get('TRAVIS_JOB_NUMBER', 'latest'))
+
+    # For BrowserStack
+    elif is_browserstack:
+        options.setdefault('build', environ.get('TRAVIS_BUILD_NUMBER', 'latest'))
+        options.setdefault('browserstack.tunnel', True)
+        options.setdefault('browserstack.debug', True)
+        options.setdefault('newSessionWaitTimeout', 120)
+        options.setdefault('Idle timeout', 120)
+        options.setdefault('platform', 'Any')
 
     if 'browserName' not in options and 'browser' in options:
         options['browserName'] = options.pop('browser')
 
     if options.get('browserName').lower() == 'ie':
-        options['browserName'] = 'Internet Explorer'
+        if is_saucelabs:
+            options['browserName'] = 'Internet Explorer'
+        else:
+            options['browserName'] = 'internet_explorer'
 
     browserName = options.get('browserName').lower()
-    if browserName == 'internet explorer':
-        assert 'version' in options
-        version = float(options['version'])
-        if version < 8:
-            options.setdefault('platform', 'Windows XP')
-        elif version < 10:
-            options.setdefault('platform', 'Windows 7')
-        else:
-            options.setdefault('platform', 'Windows 8')
+    if is_saucelabs:
+        if browserName == 'internet explorer':
+            assert 'version' in options
+            version = float(options['version'])
+            if version < 8:
+                options.setdefault('platform', 'Windows XP')
+            elif version < 10:
+                options.setdefault('platform', 'Windows 7')
+            else:
+                options.setdefault('platform', 'Windows 8')
 
-    elif browserName == 'android':
-        options.setdefault('platform', 'Linux')
+        elif browserName == 'android':
+            options.setdefault('platform', 'Linux')
 
-    elif browserName in ('iphone', 'ipad'):
-        assert 'version' in options
-        version = float(options['version'])
-        if version < 5.1:
-            options.setdefault('platform', 'OS X 10.6')
-        else:
-            options.setdefault('platform', 'OS X 10.8')
+        elif browserName in ('iphone', 'ipad'):
+            assert 'version' in options
+            version = float(options['version'])
+            if version < 5.1:
+                options.setdefault('platform', 'OS X 10.6')
+            else:
+                options.setdefault('platform', 'OS X 10.8')
+
+    if is_browserstack:
+        if browserName == 'internet explorer':
+            version = float(options.get('version', '10'))
+            options.setdefault('os', 'Windows')
+            if version < 8:
+                options.setdefault('os_version', 'XP')
+            elif version < 10:
+                options.setdefault('os_version', '7')
+            else:
+                options.setdefault('os_version', '8')
 
     # Create the test function
     @wraps(func)
