c384371cd7694746b41625976ad66f2b4a923dac
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-11-18 09:50:06 -0500

    Add a debug template helper

--- src/visionlouisville/static/js/handlebars-helpers.js
+++ src/visionlouisville/static/js/handlebars-helpers.js
@@ -3,6 +3,10 @@
 var VisionLouisville = VisionLouisville || {};
 
 (function(NS) {
+  Handlebars.registerHelper('debug', function(obj) {
+    return JSON.stringify(obj);
+  });
+
   Handlebars.registerHelper('visions_url_name', function(category) {
     return NS.appConfig.vision_plural;
   });
